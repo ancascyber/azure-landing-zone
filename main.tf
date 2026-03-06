9cbb61426bcb2cd64ba4e6aa0a1be79c9e7450a1
ancascyber <ancascyber@gmail.com>
2026-03-06 15:09:24 -0500

    first code to azure landing zone script

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-landing-zone"
+  location = "eastus"
+}
